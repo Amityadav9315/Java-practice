c44c6c351999b81918a5bf2b51fa59bdff908c37
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-15 17:29:05 +0530

    Factorial

---
+++ src/factorial/factorial.java
@@ -0,0 +1,18 @@
+package factorial;
+
+import java.util.Scanner;
+
+public class factorial {
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the number");
+            int n=sc.nextInt();
+            int i,fact=1;
+
+            for(i=1;i<=n;i++){
+                fact=fact*i;
+            }
+            System.out.println("Factorial of "+n+" is: "+fact);
+        }
+    }
+
