3fa25a59b16a6eaf9080e6c6a8556e6607db0ee0
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-15 17:20:27 +0530

    greaternum

--- src/program/greaternum.java
+++ src/program/greaternum.java
@@ -1,2 +1,54 @@
-package program;public class greaternum {
+import java.lang.*;
+import java.util.*;
+public class greaternum {
+    //if elseif condition
+
+    public static void max() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
+        System.out.println("Enter  the value of b");
+        int b = sc.nextInt();
+        System.out.println("Enter  the value of c");
+        int c = sc.nextInt();
+
+        if (a==b||b==c||a==c) {
+            System.out.println("equal");
+
+        } else if (a > b && a > c)
+            System.out.println("a is greater");
+        else if (a < b && c < b)
+            System.out.println("b is greater");
+        else
+            System.out.println("c is greater");
+
+    }
+
+    public static void exit() {
+        System.out.println("stop operation");
+        System.exit(0);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the OPERATION");
+        String str = sc.next();
+        do {
+            switch (str) {
+                case "max":
+                    max();
+                    System.out.println("Enter the OPERATION");
+                    str = sc.next();
+                    break;
+                case "exit":
+                    exit();
+                    System.out.println("Enter the OPERATION");
+                    str = sc.next();
+                    //return;
+                    break;
+            }
+
+        }
+        while (true);
+    }
 }
