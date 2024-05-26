5814987f90d99fd1368c48cc9f723e0026ad7673
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-26 13:28:57 +0530

    TOH

---
+++ src/Stack/TowerofHanoi.java
@@ -0,0 +1,25 @@
+package Stack;
+
+import java.util.*;
+
+import java.util.*;
+class TowerofHanoi {
+    static void TOH(int n, char Source, char Aux, char Dest) throws Exception
+    {
+        if(n==1){
+            System.out.println(Source+"->"+Dest);
+            return;
+        }
+        TOH(n-1,Source,Aux,Dest);
+        System.out.println(Source+"->"+Dest);
+        TOH(n-1,Aux,Dest,Source);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Entre the no of disk");
+        int n=sc.nextInt();
+        TOH(n,'A','B','C');
+
+                        }
+}
