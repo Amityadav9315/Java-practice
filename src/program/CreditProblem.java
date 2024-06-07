2248765a25a1b76f3cfa3e73135a64e98fa7c469
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-07 12:18:57 +0530

    CreditProblem

---
+++ src/program/CreditProblem.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class CreditProblem {
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc=new Scanner(System.in);
+                int x=sc.nextInt();
+                if(x>65){
+                    System.out.println("overload");
+
+                }
+                else if(x<35){
+                    System.out.println("Underload");
+                }
+                else{
+                    System.out.println("Normal");
+                }
+            }
+            // your code goes here
+
+        }
+
+
+
