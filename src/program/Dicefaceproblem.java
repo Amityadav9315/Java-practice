e24491cc96a2b18d3d4e713e5143a877bbda495c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-07 12:16:24 +0530

    DiceFaceProblem

---
+++ src/program/Dicefaceproblem.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Dicefaceproblem {
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc=new Scanner(System.in);
+            int t=sc.nextInt();
+            while(t-->0){
+                int x=sc.nextInt();
+                int d=7-x;
+                System.out.println(d);
+            }
+            // your code goes here
+
+        }
+    }
+
+
