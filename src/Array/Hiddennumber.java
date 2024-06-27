f07846d0161470e4ac7da7d3bd3112ad46dbdf15
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-27 11:34:29 +0530

    hiddenNumber

--- src/Array/Hiddennumber.java
+++ src/Array/Hiddennumber.java
@@ -1,21 +1,21 @@
 import java.util.*;
 public class Hiddennumber {
-        public static void main (String[] args) throws java.lang.Exception
-        {
-            Scanner sc=new Scanner(System.in);
-                int n=sc.nextInt();
-                int a=sc.nextInt();
-                int b=sc.nextInt();
-                if(a>0&&n>=a){
-                    System.out.println("a");
-                }
-                else if(b>0&&b<=n){
-                    System.out.println("b");
-                }
-            }
-            // your code goes here
+        public static void main (String[] args) throws java.lang.Exception {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int a = 1;
+            int b = n;
+            System.out.println(a + " " + b);
+
 
         }
-    }
+
+
+}
+
+            // your code goes here
+
+
+
 
 
