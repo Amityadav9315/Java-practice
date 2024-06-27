30ed3236c2ab13bb22fd8c6ea144700f61958c15
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-06-27 11:55:24 +0530

    OlympicRanking

---
+++ src/Array/OlympicRanking.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class OlympicRanking {
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc=new Scanner(System.in);
+                int g1=sc.nextInt();
+                int s1=sc.nextInt();
+                int b1=sc.nextInt();
+                int g2=sc.nextInt();
+                int s2=sc.nextInt();
+                int b2=sc.nextInt();
+                int c=g1+b1+s1;
+                int d=g2+b2+s2;
+                if(c>d){
+                    System.out.println("1");
+                }
+                else{
+                    System.out.println("2");
+                }
+            }
+            // your code goes here
+
+        }
+
+
+
