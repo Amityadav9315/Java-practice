33c7e3ebb0ca17d564f518201430e05dd9eda1ba
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-01 20:04:53 +0530

    BuyingnewTable

--- src/Array/BuyingnewTablet.java
+++ src/Array/BuyingnewTablet.java
@@ -4,6 +4,7 @@ public class BuyingnewTablet {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
             int n = sc.nextInt();
             int b = sc.nextInt();
             int maxArea = 0;
