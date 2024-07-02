8a08e1fe1cf22ad8cdda75af27162ff0434ffbb4
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-02 23:30:04 +0530

    Hiddennumber

--- src/Array/Hiddennumber.java
+++ src/Array/Hiddennumber.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Hiddennumber {
         public static void main (String[] args) throws java.lang.Exception {
             Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number");
             int n = sc.nextInt();
             int a = 1;
             int b = n;
