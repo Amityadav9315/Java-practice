62ce3423a3f04435660d7629d7415aa9d88bec4f
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-07-03 12:05:51 +0530

    FarmerandHisplot

---
+++ src/Array/FarmerandhidPlot.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class FarmerandhidPlot {
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            Scanner sc=new Scanner(System.in);
+            int t=sc.nextInt();
+            while(t-->0){
+                int n=sc.nextInt();
+                int m=sc.nextInt();
+                int c=0;
+                if(n>m){
+                    c=n-m;
+                }
+                else{
+                    c=m-n;
+                }
+                int e=n*m;
+                int d=c*c;
+                System.out.println(e/d);
+            }
+            // your code goes here
+
+        }
+    }
+
+
